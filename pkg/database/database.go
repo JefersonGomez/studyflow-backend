694709b4c8357516877658e4a91f47507a74f420
JefersonGomez <gomesjeje504@gmail.com>
2026-06-28 23:38:34 -0600

    configuracion de DB para neon

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -13,22 +13,28 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+	var dsn string
 
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+	if os.Getenv("ENV") == "production" {
+		// Neon usa connection string directa
+		dsn = os.Getenv("DB_URL")
+	} else {
+		// Local usa variables separadas
+		dsn = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+		)
+	}
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
-
-		log.Fatal("Error al conectar a la base de datos", err)
+		log.Fatal("Error al conectar a la base de datos: ", err)
 	}
+
 	DB = db
-	log.Println("Conectado a postgress")
+	log.Println("Conectado a PostgreSQL")
 }
